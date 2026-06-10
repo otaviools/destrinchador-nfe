9e52c8d43b40f7c7d0c3f7dfb0112519e0d8e77e
Otavio Santos <otavio.santos@supplog.com>
2026-06-10 14:59:19 -0300

    feat(Destrinchador): add the components nuxtui header and footer

---
+++ app/components/AppFooter.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import type { NavigationMenuItem } from '@nuxt/ui'
+</script>
+
+<template>
+  <div class="h-100dvh">
+    <template>
+      <UFooter>
+        <template #left>
+          <p class="text-muted text-sm">Otávio © Todos os direitos reservados.</p>
+        </template>
+
+        <template #right>
+          <UButton
+            icon="i-simple-icons-discord"
+            color="neutral"
+            variant="ghost"
+            to="https://go.nuxt.com/discord"
+            target="_blank"
+            aria-label="Discord"
+          />
+          <UButton
+            icon="i-simple-icons-x"
+            color="neutral"
+            variant="ghost"
+            to="https://go.nuxt.com/x"
+            target="_blank"
+            aria-label="X"
+          />
+          <UButton
+            icon="i-simple-icons-github"
+            color="neutral"
+            variant="ghost"
+            to="https://github.com/nuxt/nuxt"
+            target="_blank"
+            aria-label="GitHub"
+          />
+        </template>
+      </UFooter>
+    </template>
+  </div>
+</template>
+
+<style></style>
